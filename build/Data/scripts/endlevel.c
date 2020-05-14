547acf2b4d3eecb61cd92fd8464b7eacbd7988f9
Digital Brilliance Hour <digitalbrilliancehour@gmail.com>
2020-05-14 19:01:54 -0400

    fixed lvl text - mr. epps

--- build/Data/scripts/endlevel.c
+++ build/Data/scripts/endlevel.c
@@ -3,4 +3,20 @@ void main()
 {
 	clearStory();
 	setglobalvar("zoomentity", NULL());
+
+	void p;  
+   int i, hp, num;  
+   for(i=0; i<4; i++){  
+    p = getplayerproperty(i, "entity");  
+    hp = getentityproperty(p, "health");
+    num = i+1;  
+    if(p){    
+     if(getglobalvar("saved_health_"+num+"P") == NULL()){  
+        setglobalvar("saved_health_"+num+"P", hp); 
+     }  
+    }  
+   }  
+
+
+
 }
--- build/Data/scripts/level.c
+++ build/Data/scripts/level.c
@@ -1,4 +1,20 @@
 void main()
 {
    setglobalvar("zoomentity", NULL());
+
+	void p;
+    int i, hp, num, stage;
+	for(i=0; i<4; i++){
+     	p = getplayerproperty(i, "entity");
+		num = i+1;
+    	if(p){	
+    		stage = openborvariant("current_stage");
+    		if(stage % 3 != 0) {
+	    		hp = getglobalvar("saved_health_"+num+"P");
+				if(hp > 0){ 
+	     			changeentityproperty(p, "health", hp);
+	    		}
+	    	}
+    	}
+	}
 }
