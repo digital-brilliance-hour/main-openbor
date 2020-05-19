c8e59192b4f858d21be1ba6d40a4c69a48b4825b
DBH Coder <coder@digitalbrilliancehour.com>
2020-05-19 18:36:26 -0400

    added health functionality - william

--- build/Data/scripts/level.c
+++ build/Data/scripts/level.c
@@ -1,4 +1,22 @@
 void main()
 {
    setglobalvar("zoomentity", NULL());
+
+	 void p;
+	 int i, hp, num, stage, maxhp, newhp;
+	for(i=0; i<4; i++){
+		 p = getplayerproperty(i, "entity");
+		num = i+1;
+		 if(p){
+			 stage = openborvariant("current_stage");
+			 if(stage % 3 != 0) {
+				 hp = getglobalvar("saved_health_"+num+"P");
+				if(hp > 0){
+					maxhp = getentityproperty(p, "maxhealth");
+					newhp = maxhp/2;
+					changeentityproperty(p, "health", hp+newhp);
+				 }
+	 		}
+	 	}
+	}  
 }
