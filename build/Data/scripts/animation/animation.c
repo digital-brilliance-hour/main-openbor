eaa43ab03e1821846ddfc80d51a82236486e56f3
DBH Coder <reanimixtv@gmail.com>
2021-04-29 17:48:50 -0400

    latest update -ke

--- build/Data/scripts/animation/animation.c
+++ build/Data/scripts/animation/animation.c
@@ -2002,3 +2002,30 @@ changedrawmethod(self, "channelg", 64);
 changedrawmethod(self, "channelb", 0);
 //changedrawmethod(self, "gfxshadow", 0);
 }
+
+void pause_all(int iToggle, int iTime){
+
+    /*
+    paus0001
+    Damon Vaughn Caskey
+    11022009
+    Pause or unpause action for all entities except self.
+    */
+
+    void vSelf      = getlocalvar("self");                  //Caller   
+    int  iETime     = openborvariant("elapsed_time");       //Current time.
+    int  iMax       = openborvariant("ent_max");            //Entity count.
+    int  iEntity;                                           //Loop counter.
+    void vEntity;                                           //Target entity.
+
+    for(iEntity=0; iEntity<iMax; iEntity++)
+    {   
+        vEntity = getentity(iEntity);                                       //Get target entity from current loop.       
+       
+        if (vEntity != vSelf)                                               //Not Self?
+        {
+            changeentityproperty(vEntity, "frozen", iToggle);               //Toggle frozen.
+            changeentityproperty(vEntity, "freezetime", iETime + iTime);    //Toggle frozen time.
+        }
+    }   
+}
